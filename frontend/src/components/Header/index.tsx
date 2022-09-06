7dfd2dc0a06517431392f9ecbc2a846cd4e54ca0
josemontalvao <jokmontalvao@gmail.com>
2022-09-06 13:12:20 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import logo from '../../assets/img/logo.svg'
+import './styles.css'
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="dsmeta" />
+                    <h1>DSMeta</h1>
+                    <p>Desenvolvido por <a
+                        href="https://www.linkedin.com/in/jos%C3%A9-antonio-montalv%C3%A3o-aa16aa92/">@JoséAntonioMontalvão.in</a>
+                    </p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
